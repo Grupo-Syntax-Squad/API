0c6bedbb6882dfd9911f6096caf5621890e4498a
Wellington Luiz de Faria <91472871+WellingtonLFaria@users.noreply.github.com>
2023-05-12 15:34:00 -0300

    Adicionado gráfico 5 maiores gastos Caçapava

---
+++ src/Gráficos/Gastos/Caçapava/graficogastos.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import plotly.express as px
+
+MESES = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+ANOS = ["2019", "2020", "2021", "2022"]
+
+valoresanos = []
+
+for ano in ANOS:
+    valores = []
+    df = pd.read_excel(f'C:/Users/Wellington/Documents/GitHub/API/src/Gráficos/Gastos/Caçapava/Gastos{ano}/{ano}.xlsx')
+    total = df.sort_values('Quanto já foi realizado <br>(Empenhado ou Pago)')
+    total.reset_index(drop=True, inplace=True)
+    for valor in total['Quanto já foi realizado <br>(Empenhado ou Pago)'][-5:]:
+        valores.append(valor)
+    print(ano)
+    print(sorted(valores, reverse=True))
+    valoresanos.append(sorted(valores, reverse=True))
+
+valoresanos = pd.DataFrame(valoresanos, index=ANOS)
+
+fig = px.bar(valoresanos, text_auto=True, barmode="group", width=700, height=600, title="5 maiores gastos por ano de Caçapava", labels={
+                     "value": "Gastos (R$)",
+                     "index": "Anos"
+                 })
+
+fig.update_layout(showlegend=False, title_x=0.5)
+fig.update_traces(textposition="inside")
+
+fig.show()
+
+fig.write_image('C:/Users/Wellington/Documents/GitHub/API/src/Gráficos/Gastos/Caçapava/GráficoGastosporAnoCaçapava.svg')
