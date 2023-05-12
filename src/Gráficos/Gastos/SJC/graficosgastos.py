4e417855f487985e8214ad8e29f856ca1b03e565
Wellington Luiz de Faria <91472871+WellingtonLFaria@users.noreply.github.com>
2023-05-11 21:56:29 -0300

    Raspagem dos gastos SJC

---
+++ src/Gráficos/Gastos/SJC/graficosgastos.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+ANOS = ['2019', '2020', '2021', '2022']
+valores = []
+
+"""for ano in ANOS:
+    for c in range(12):
+        try:
+            if c == 0:
+                df = pd.read_csv(f'src/Gráficos/Gastos/SJC/Gastos{ano}/despesa_funcao.csv', sep=';', encoding='latin', on_bad_lines='skip')
+            else:
+                df = pd.read_csv(f'src/Gráficos/Gastos/SJC/Gastos{ano}/despesa_funcao ({c}).csv', sep=';', encoding='latin', on_bad_lines='skip')
+            print(ano, c+1)
+            df['Pago  (R$)'].to_csv(f'src/Gráficos/Gastos/SJC/Gastos{ano}/{c+1}-{ano}.csv')
+        except:
+            pass"""
+
+for ano in ANOS:
+    for c in range(12):
+        try:
+            with open(f'src/Gráficos/Gastos/SJC/Gastos{ano}/{c+1}-{ano}.csv') as arquivo:
+                print(arquivo.read().split(','))
+        except:
+            pass
--- src/Gráficos/Gastos/SJC/raspagemgastos.py
+++ src/Gráficos/Gastos/SJC/raspagemgastos.py
@@ -3,11 +3,45 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
+import os
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://servicos.sjc.sp.gov.br/portal_da_transparencia/despesa_funcao.aspx")
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+anos = ['2022']
 
-select = driver.find_element(By.NAME, "ctl00$cphConteudo$cmbPeriodo")
+for ano in anos:
+    pasta = f"C:/Users/Wellington/Documents/GitHub/API/src/Gráficos/Gastos/SJC/Gastos{ano}"
 
-select.select_by_value('202305')
+    # Vê se a pasta do respectivo ano existe
+    if not os.path.exists(pasta):
+        # Se não existir cria
+        os.makedirs(pasta)
+
+    for mes in meses:
+        data = f"{mes}/{ano}"
+        # Abrindo o portal de transparência
+        driver.get("https://servicos.sjc.sp.gov.br/portal_da_transparencia/despesa_funcao.aspx")
+
+        # Encontrando o menu dropdown
+        select = driver.find_element(By.NAME, "ctl00$cphConteudo$cmbPeriodo")
+
+        # Clicando no menu
+        select.click()
+
+        try:
+            # Encontrando e clicando no mês desejado.
+            driver.find_element(By.XPATH, f"//option[.='{data}']").click()
+
+            # Encontrando e clicando no link para exportar os dados.
+            driver.find_element(By.ID, 'ctl00_cphConteudo_lnkExportarLista').click()
+            time.sleep(1)
+        except:
+            pass
+    
+    for c in range(12):
+        if c == 0:
+            os.replace('C:/Users/Wellington/Downloads/despesa_funcao.csv', f'C:/Users/Wellington/Documents/GitHub/API/src/Gráficos/Gastos/SJC/Gastos{ano}/despesa_funcao.csv')
+        else:
+            os.replace(f'C:/Users/Wellington/Downloads/despesa_funcao ({c}).csv', f'C:/Users/Wellington/Documents/GitHub/API/src/Gráficos/Gastos/SJC/Gastos{ano}/despesa_funcao ({c}).csv')
