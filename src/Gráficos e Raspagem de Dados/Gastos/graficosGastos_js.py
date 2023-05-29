c53b94b44948b516fc6dda9f1339c3cde1210359
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-05-29 16:41:42 -0300

    Adicionado script para coletar os dados de Gastos para integrar com o JS

--- src/Gráficos e Raspagem de Dados/Gastos/graficosGastos_js.py
+++
@@ -1,4 +0,0 @@
-CIDADES = ['Aparecida', 'Caçapava', 'Jacareí', 'SJC', 'Taubaté']
-def maioresgastos():
-    """Retorna um dicionário com os 5 maiores gastos de cada ano de cada cidade.
-    """
---
+++ src/graficosGastos_js.py
@@ -0,0 +1,16 @@
+import os
+
+CIDADES = ['Aparecida', 'Caçapava', 'Jacareí', 'SJC', 'Taubaté']
+def maioresgastos():
+    """Retorna um dicionário com os 5 maiores gastos de cada ano de cada cidade.
+    """
+    gastos = {}
+    for cidade in CIDADES:
+        cidadegastos = []
+        with open(f"{os.path.dirname(os.path.abspath(__file__))}/Gráficos e Raspagem de Dados/Gastos/{cidade}/5maioresgastos.csv", "r") as arquivo:
+            for linha in arquivo.readlines():
+                cidadegastos.append(linha)
+        gastos[cidade] = cidadegastos
+    print(gastos)
+
+maioresgastos()
