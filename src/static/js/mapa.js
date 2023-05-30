72bfcbb9328179b8048f2bbb2eede026fb6ad9b6
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2023-05-30 18:17:06 -0300

    Adicionei uma nova função ao mapa.js para mudar a cor dos textos

--- src/static/js/mapa.js
+++ src/static/js/mapa.js
@@ -1,12 +1,32 @@
 //Arquivo Javascript para edição do mapa interativo
 
 let areas = document.querySelectorAll('#mun_3549904, #mun_3502507, #mun_3508504, #mun_3554102, #mun_3524402');
+let textIds = ['text3947', 'text3957', 'text4005', 'text3997', 'text4081'];
 
 areas.forEach(function(area){
     area.addEventListener('mouseover', function(){
-    area.style.fill = '#1B5161'
+    area.style.fill = '#1B5161';
+
+    textIds.forEach(function(textId) {
+        const texto = document.getElementById(textId);
+        if (texto) {
+          texto.style.fill = '#ffffff';
+        }
+      });
     });
     area.addEventListener('mouseout', function(){
         area.style.fill = '#ffffff'
-        })
-    });
+        textIds.forEach(function(textId) {
+            const texto = document.getElementById(textId);
+            if (texto) {
+              texto.style.fill = '#000000';
+            }
+          });
+        });
+      });
+
+// Selecionando o elemento de texto pelo ID
+
+
+// Mudando a cor da fonte para vermelho
+
