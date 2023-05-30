e1406fed03541df248a50896857e3e7e4a5f3f24
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2023-05-29 22:47:58 -0300

    Corrigi a cor do mapa da página inicial e alguns erros ortográficos

--- src/static/js/mapa.js
+++ src/static/js/mapa.js
@@ -1,10 +1,10 @@
-//Arquivo Javasccript para edição do mapa interativo
+//Arquivo Javascript para edição do mapa interativo
 
 let areas = document.querySelectorAll('#mun_3549904, #mun_3502507, #mun_3508504, #mun_3554102, #mun_3524402');
 
 areas.forEach(function(area){
     area.addEventListener('mouseover', function(){
-    area.style.fill = '#2b8692'
+    area.style.fill = '#1B5161'
     });
     area.addEventListener('mouseout', function(){
         area.style.fill = '#ffffff'
