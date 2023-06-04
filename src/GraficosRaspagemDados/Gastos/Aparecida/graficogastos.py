2e916fb2dcbfc44e167b6efbed184b444c9379f9
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-06-04 17:47:20 -0300

    Adicionado legenda dos gráficos de 5 maiores gastos de Aparecida e Jacareí

--- src/GraficosRaspagemDados/Gastos/Aparecida/graficogastos.py
+++ src/GraficosRaspagemDados/Gastos/Aparecida/graficogastos.py
@@ -10,17 +10,12 @@ valoresanos = []
 
 for ano in ANOS:
     valores = []
-    df = pd.read_csv(f'C:\\Users\\Wellington\\Documents\\Trampos\\API\\src\\GraficosRaspagemDados\\Gastos\\Aparecida\\Gastos{ano}\\detalhamentoDespesas{ano}.csv', sep=';')
+    df = pd.read_csv(f'{os.getcwd()}/Gastos{ano}/detalhamentoDespesas{ano}.csv', sep=';')
     total = df.sort_values('vl_total_pag')
-    totalk = df[['natureza', 'elemento', 'vl_total_pag']]
-    totalk.reset_index(drop=True, inplace=True)
     total.reset_index(drop=True, inplace=True)
-    
-    print((totalk["natureza"], totalk['elemento'], totalk['vl_total_pag']))
-    valores = [valor for valor in total['vl_total_pag'][-5:]]
+    for valor in total['vl_total_pag'][-5:]:
+        valores.append(valor)
     valoresanos.append(sorted(valores, reverse=True))
-
-print(valoresanos)
 
 if os.path.exists(f"5maioresgastos.csv"):
     os.remove(f"5maioresgastos.csv")
@@ -29,16 +24,16 @@ with open("5maioresgastos.csv", "a") as arquivo:
     for ano in valoresanos:
         arquivo.write(f"{ano}\n")
 
-# valoresanos = pd.DataFrame(valoresanos, index=ANOS)
+valoresanos = pd.DataFrame(valoresanos, index=ANOS)
 
-# fig = px.bar(valoresanos, text_auto=True, barmode="group", width=700, height=600, title="5 maiores gastos por ano de Aparecida", labels={
-#                      "value": "Gastos (R$)",
-#                      "index": "Anos"
-#                  })
+fig = px.bar(valoresanos, text_auto=True, barmode="group", width=700, height=600, title="5 maiores gastos por ano de Aparecida", labels={
+                     "value": "Gastos (R$)",
+                     "index": "Anos"
+                 })
 
-# fig.update_layout(showlegend=False, title_x=0.5)
-# fig.update_traces(textposition="inside")
+fig.update_layout(showlegend=False, title_x=0.5)
+fig.update_traces(textposition="inside")
 
-# fig.show()
+fig.show()
 
-# fig.write_image('src/Gráficos e Raspagem de Dados/Gastos/Aparecida/GráficoGastosporAnoAparecida.svg')
+fig.write_image('src/Gráficos e Raspagem de Dados/Gastos/Aparecida/GráficoGastosporAnoAparecida.svg')
